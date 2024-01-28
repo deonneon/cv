ebd512a1ba001997d4c32949d02996b0283712f1
deonneon <dtnifti@gmail.com>
2024-01-28 11:53:27 -0600

    committed

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -7,7 +7,7 @@ export default function ProjectsPage() {
   return (
     <main className="container mx-auto p-4">
       <Section className="space-y-4">
-        {/* <h2 className="text-xl font-bold">Projects</h2> */}
+        <h2 className="text-xl">Pledged Open-Sourced and Work In Progress</h2>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {RESUME_DATA.projects.map((project) => (
             <ProjectCard
